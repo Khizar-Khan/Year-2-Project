88bd5f9f876835754a26bfcadfb969ed070b526f
Khizar Khan <1808740@Brunel.ac.uk>
2019-12-27 13:07:13 +0000

    Registering now stores user data into firebase.

--- app/src/main/java/com/khizar/year2groupproject/ProfileActivity.java
+++ app/src/main/java/com/khizar/year2groupproject/ProfileActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
--- app/src/main/java/com/khizar/year2groupproject/Register.java
+++ app/src/main/java/com/khizar/year2groupproject/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +15,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity
 {
+    public static final String TAG = "TAG";
     EditText mFullName, mEmail, mPassword, mPhone;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
 
     @Override
@@ -41,6 +51,7 @@ public class Register extends AppCompatActivity
         mLoginBtn = findViewById(R.id.loginText);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
         if(fAuth.getCurrentUser() != null)
@@ -56,8 +67,9 @@ public class Register extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                String email = mEmail.getText().toString();
+                final String email = mEmail.getText().toString();
                 String password = mPassword.getText().toString();
+                final String fullName = mFullName.getText().toString();
 
                 if(TextUtils.isEmpty(email))
                 {
@@ -73,6 +85,7 @@ public class Register extends AppCompatActivity
                 if(password.length() < 6)
                 {
                     mPassword.setError("Password must be 6 or more characters!");
+                    return;
                 }
                 progressBar.setVisibility(View.VISIBLE);
 
@@ -85,6 +98,19 @@ public class Register extends AppCompatActivity
                         if(task.isSuccessful())
                         {
                             Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fName", fullName);
+                            user.put("email", email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>()
+                            {
+                                @Override
+                                public void onSuccess(Void aVoid)
+                                {
+                                    Log.d(TAG,"onSuccess: user Profile is created for "+ userID);
+                                }
+                            });
                             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                             startActivity(intent);
                         }
