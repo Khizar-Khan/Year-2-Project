061aca3941e32b932914fc6120868b3f0e5ab3cf
MZNK <1706960@brunel.ac.uk>
2020-01-27 04:07:16 +0000

    Added 'going back a page' functionality on the webView.

--- app/src/main/java/com/khizar/year2groupproject/ChatActivity.java
+++ app/src/main/java/com/khizar/year2groupproject/ChatActivity.java
@@ -3,6 +3,7 @@ package com.khizar.year2groupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -17,7 +18,15 @@ public class ChatActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://webapps.brunel.ac.uk/apps/jobs-search/UI/pages/results.aspx?sysSection=2&sort=datecreated&rpp=10&type=1+Year+Placement&rel=Dept+of+Computer+Science&filter=2");
+
     }
 
-
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
