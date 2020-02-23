52a0da8dce6ff6a36447b28b28dd79e23f0287cc
vshaji <48948015+vshaji@users.noreply.github.com>
2020-02-23 17:35:50 +0000

    Created the assessment resources screen which displays all the modules that have assessment tasks along with three resources including the assessment brief that will be useful in completing the asssessment task
    Created the assessment resources screen which displays all the modules that have assessment tasks along with three resources including the assessment brief that will be useful in completing the asssessment task

---
+++ app/src/main/java/com/khizar/year2groupproject/AssessmentResourceActivity.java
@@ -0,0 +1,14 @@
+package com.khizar.year2groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AssessmentResourceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment_resource);
+    }
+}
