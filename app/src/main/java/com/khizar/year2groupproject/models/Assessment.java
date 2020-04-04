6bf54d17fd58c538b12f2fd4751170bb3ed32050
Edward Akkawi <edward.akkawi@gmail.com>
2020-04-04 12:46:11 +0300

    Refactor Assessment method to get assessments before deadline

--- app/src/main/java/com/khizar/year2groupproject/models/Assessment.java
+++ app/src/main/java/com/khizar/year2groupproject/models/Assessment.java
@@ -1,13 +1,19 @@
 package com.khizar.year2groupproject.models;
 
+import com.khizar.year2groupproject.MySQLConnector;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Assessment {
+    private static final int offset = 3;
     private String Code;
     private String Title;
     private String Type;
     private Date Deadline;
-    private String Percentage;
+    private String Weight;
 
     public String getCode() {
         return Code;
@@ -28,8 +34,8 @@ public class Assessment {
         return Deadline;
     }
 
-    public String getPercentage() {
-        return Percentage;
+    public String getWeight() {
+        return Weight;
     }
 
     public String getType() {
@@ -40,11 +46,45 @@ public class Assessment {
         Deadline = deadline;
     }
 
-    public void setPercentage(String percentage) {
-        Percentage = percentage;
+    public void setWeight(String weight) {
+        Weight = weight;
     }
 
     public void setType(String type) {
         Type = type;
+    }
+
+    public static ArrayList<Assessment> GetBeforeDeadline(MySQLConnector connector, Date date)
+    {
+        ArrayList<String> data = connector.readAssessmentInformation();
+        ArrayList<Assessment> assessments = new ArrayList<Assessment>();
+
+        for(int i = 0; i < data.size(); i+= offset)
+        {
+            Assessment assessment = new Assessment();
+            String type = data.get(i);
+            String deadline = data.get(i + 1);
+            String weight = data.get(i + 2);
+
+            try
+            {
+                Date deadlineDate = new SimpleDateFormat("dd/MM/yyyy")
+                        .parse(deadline);
+                if (deadlineDate.compareTo(date) < 0)
+                {
+                    assessment.setType(type);
+                    assessments.add(assessment);
+                    assessment.setDeadline(deadlineDate);
+                    assessment.setWeight(weight + "%");
+                }
+            }
+            catch(ParseException ex)
+            {
+                //ignore assessments with invalid dates
+            }
+
+        }
+
+        return assessments;
     }
 }
